2ed507773e1e09c795f992406db8edb737d9c96b
Jeff Bai <j.bai@wustl.edu>
2020-05-13 10:59:34 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -18,10 +18,10 @@ def fourth_challenge
          {name: "Benvolio", 
          age: "17", 
          attitude: "worried"},
-         {name: "Mercutio", 
+         {
+         name: "Mercutio", 
          age: "18", 
          attitude: "hot-headed"
-           
          }
       ]
    },
