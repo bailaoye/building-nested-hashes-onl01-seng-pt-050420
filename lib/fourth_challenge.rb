e4a8d77419c392816e48b560ab4d63a027ede2aa
Jeff Bai <j.bai@wustl.edu>
2020-05-13 10:59:25 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -15,8 +15,14 @@ def fourth_challenge
         status: "alive"
       },
       :hero_friends => [
-         {name: "Benvolio", age: "17", attitude: "worried"},
-         {name: "Mercutio", age: "18", attitude: "hot-headed"}
+         {name: "Benvolio", 
+         age: "17", 
+         attitude: "worried"},
+         {name: "Mercutio", 
+         age: "18", 
+         attitude: "hot-headed"
+           
+         }
       ]
    },
    :capulet => {
